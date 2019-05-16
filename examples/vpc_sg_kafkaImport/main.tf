13f473ca22227c42c327cead4242076357c5afd6
ssomagani <seeta.somagani@gmail.com>
2019-05-16 00:53:08 -0400

    Cleaned up the security group rules

--- examples/vpc_sg_kafkaImport/main.tf
+++ examples/vpc_sg_kafkaImport/main.tf
@@ -13,39 +13,60 @@ module "vpc" {
 	map_public_ip_on_launch = "true"
 }
 
-resource "aws_security_group" "allow_22_8080_3021" {
-  name        = "allow_22_8080_3021"
-  description = "Allow all inbound TCP traffic on ports 22, 8080 and subnet-only traffic on 3021"
+resource "aws_security_group" "voltdb_public" {
+  name        = "voltdb_public"
+  description = "Allow public traffic into ports 22, 5555, 8443, 80890, 21211, and 21212"
   vpc_id      = "${module.vpc.vpc_id}"
 
+  # Public ports
+  # SSH
   ingress {
     from_port   = "22"
     to_port     = "22"
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  # XDCR
+  ingress {
+    from_port   = "5555"
+    to_port     = "5555"
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    
+  # TLS
+  ingress {
+    from_port   = "8443"
+    to_port     = "8443"
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   
+  # HTTP
   ingress {
     from_port   = "8080"
     to_port     = "8080"
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
+  # Admin
   ingress {
-  	from_port   = "3021"
-    to_port     = "3021"
+  	from_port   = "21211"
+    to_port     = "21211"
     protocol    = "TCP"
-    cidr_blocks = ["${var.subnet_cidr}"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   
+  # Client
   ingress {
-  	from_port   = "9092"
-    to_port     = "9092"
+  	from_port   = "21212"
+    to_port     = "21212"
     protocol    = "TCP"
-    cidr_blocks = ["${var.subnet_cidr}"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  
+      
   egress {
     from_port       = "0"
     to_port         = "0"
@@ -54,7 +75,34 @@ resource "aws_security_group" "allow_22_8080_3021" {
   }
   
   tags = {
-  	Name = "allow_22_8080_3021"
+  	Name = "voltdb_public"
+  }
+}
+
+# Private ports
+resource "aws_security_group" "voltdb_private" {
+  name        = "voltdb_private"
+  description = "Allow subnet-only traffic on ports 3021"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  # Internal Server  
+  ingress {
+  	from_port   = "3021"
+    to_port     = "3021"
+    protocol    = "TCP"
+    cidr_blocks = ["${var.subnet_cidr}"]
+  }
+  
+  # Kafka
+  ingress {
+  	from_port   = "9092"
+    to_port     = "9092"
+    protocol    = "TCP"
+    cidr_blocks = ["${var.subnet_cidr}"]
+  }
+  
+  tags = {
+  	Name = "voltdb_private"
   }
 }
 
@@ -64,12 +112,12 @@ data "template_file" "ips_1" {
 }
 
 module "volt" {
-	source 				= "ssomagani/voltdb/aws"
+	source 				= "../../"
 	zone			 	= "us-east-1b"
 	subnet_id 			= "${element(module.vpc.public_subnets, 0)}"
 	subnet_cidr_block		= "${var.subnet_cidr}"
 	ip_start_offset			= "${var.ip_start_offset}"
-	security_group_id 		= "${aws_security_group.allow_22_8080_3021.id}"
+	security_group_ids 		= "${aws_security_group.voltdb_private.id};${aws_security_group.voltdb_public.id}"
 	host_string_self			= "${join(",", data.template_file.ips_1.*.rendered)}"
 	ami					= "${var.ami}"
 	node_count			= "${var.node_count}"
@@ -89,7 +137,7 @@ resource "aws_instance" kafka {
   key_name			= "${var.key_name}"
   private_ip 			= "${cidrhost(var.subnet_cidr, 20)}"
   subnet_id			= "${element(module.vpc.public_subnets, 0)}"
-  vpc_security_group_ids 	= ["${aws_security_group.allow_22_8080_3021.id}"]
+  vpc_security_group_ids 	= ["${aws_security_group.voltdb_public.id}"]
 
   provisioner "file" {
     source      		= "start-kafka.sh"
